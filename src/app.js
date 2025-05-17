c803633175f9f1aaf022b4425a92b913ca1c4646
Alex Eremin <eremin_inc@mail.ru>
2025-05-17 16:05:29 +0300

    Added context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,43 @@
-import './styles.css'
+import './styles.css';
+import { ContextMenu } from './menu.js';
 
+const menu = new ContextMenu('#menu');
+
+menu.add({
+  text: 'Считать клики (за 3 секунды)',
+  onClick: () => {
+    console.log('Считать клики (за 3 секунды)');
+  },
+});
+
+menu.add({
+  text: 'Создать фигуру',
+  onClick: () => {
+    console.log('Создать фигуру');
+  },
+});
+menu.add({
+  text: 'Таймер отсчета',
+  onClick: () => {
+    console.log('Таймер отсчета');
+  },
+});
+menu.add({
+  text: 'Кастомное сообщение',
+  onClick: () => {
+    console.log('Кастомное сообщение');
+  },
+});
+
+menu.add({
+  text: 'Случайный фон',
+  onClick: () => {
+    console.log('Случайный фон');
+  },
+});
+menu.add({
+  text: 'Случайный звук',
+  onClick: () => {
+    console.log('Случайный звук');
+  },
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,46 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu.js';
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.init();
+  }
 
+  init() {
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+      this.open(event.clientX, event.clientY);
+    });
+
+    document.body.addEventListener('click', (event) => {
+      if (event.target.offsetParent !== this.el) {
+        this.close();
+      }
+    });
+  }
+
+  open(x, y) {
+    this.el.style.display = 'block';
+    this.el.style.left = `${x}px`;
+    this.el.style.top = `${y}px`;
+  }
+
+  close() {
+    this.el.style.display = 'none';
+  }
+
+  add(item) {
+    const menuItem = document.createElement('li');
+    menuItem.className = 'menu-item';
+    menuItem.textContent = item.text;
+
+    if (item.onClick) {
+      menuItem.addEventListener('click', () => {
+        item.onClick();
+        this.close();
+      });
+    }
+
+    this.el.appendChild(menuItem);
+  }
 }
