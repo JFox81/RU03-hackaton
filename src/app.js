a46c4351d9aefcd0048cfafeaa665cab3365a2ca
Artyom <feros2014@gmail.com>
2025-05-18 20:04:22 +0700

    Создал модуль аналитики кликов

--- src/app.js
+++ src/app.js
@@ -1,61 +1,63 @@
-import './styles.css';
-import { ContextMenu } from './menu.js';
-import { TimerMoudle } from './modules/timer.module.js';
-import { PaitingModule } from './modules/paiting.module.js';
-import { BackgroundModule } from './modules/background.module.js';
-import { CustomMessageModule } from './modules/custom-message.module.js';
-import { VladilenModule } from './modules/vladilen.module.js';
-import { ShapeModule } from './modules/shape.module.js';
+import "./styles.css";
+import { ContextMenu } from "./menu.js";
+import { TimerMoudle } from "./modules/timer.module.js";
+import { PaitingModule } from "./modules/paiting.module.js";
+import { BackgroundModule } from "./modules/background.module.js";
+import { CustomMessageModule } from "./modules/custom-message.module.js";
+import { VladilenModule } from "./modules/vladilen.module.js";
+import { ShapeModule } from "./modules/shape.module.js";
+import { ClickAnalyticsModule } from "./modules/click-analytics.module.js";
 
-const menu = new ContextMenu('#menu');
+const menu = new ContextMenu("#menu");
 const customMessage = new CustomMessageModule();
-const timer = new TimerMoudle('count_timer', 'Таймер отсчета');
-const painting = new PaitingModule('painting', 'Граффити-стена');
+const timer = new TimerMoudle("count_timer", "Таймер отсчета");
+const painting = new PaitingModule("painting", "Граффити-стена");
 const backgroundModule = new BackgroundModule();
 const vladilenModule = new VladilenModule();
-const shapeModule = new ShapeModule('Создать случайную фигуру');
+const shapeModule = new ShapeModule("Создать случайную фигуру");
+const ClickAnalytics = new ClickAnalyticsModule();
 
 menu.add({
-  text: 'Считать клики (за 3 секунды)',
+  text: "Считать клики (за 3 секунды)",
   onClick: () => {
-    console.log('Считать клики (за 3 секунды)');
+    ClickAnalytics.trigger();
   },
 });
 
 menu.add({
-  text: 'Создать случайную фигуру',
+  text: "Создать случайную фигуру",
   onClick: () => {
     shapeModule.trigger();
   },
 });
 
 menu.add({
-  text: 'Таймер отсчета',
+  text: "Таймер отсчета",
   onClick: () => {
     timer.trigger();
   },
 });
 menu.add({
-  text: 'Кастомное сообщение',
+  text: "Кастомное сообщение",
   onClick: () => {
     customMessage.trigger();
   },
 });
 
 menu.add({
-  text: 'Случайный фон',
+  text: "Случайный фон",
   onClick: () => {
     backgroundModule.trigger();
   },
 });
 menu.add({
-  text: 'Only for Vladilen Minin',
+  text: "Only for Vladilen Minin",
   onClick: () => {
     vladilenModule.trigger();
   },
 });
 menu.add({
-  text: 'Граффити - стена',
+  text: "Граффити - стена",
   onClick: () => {
     painting.trigger();
   },
